348cd6107db83140668adf35f1469c64d70aba6a
qijin <qijin@f2r4s13.paris.42.school>
2026-09-23 20:31:16 +0200

    creat structure

---
+++ includes/cub3d.h
@@ -0,0 +1,16 @@
+#ifndef CUB3D_H
+# define CUB3D
+
+typedef struct s_config
+{
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+	int		floor_color;
+	int		ceiling_color;
+	char	**map;
+}	t_config;
+
+
+#endif
