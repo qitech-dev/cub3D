cb314383c779a50bee294ff3a899ac523ea56ba3
qijin <qijin@f2r4s13.paris.42.school>
2026-09-23 21:03:27 +0200

    add config in cub3.h

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,5 +1,9 @@
 #ifndef CUB3D_H
-# define CUB3D
+# define CUB3D_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 typedef struct s_config
 {
@@ -10,7 +14,15 @@ typedef struct s_config
 	int		floor_color;
 	int		ceiling_color;
 	char	**map;
+	int		map_width;
+	int		map_height;
+	int		player_x;
+	int		player_y;
+	char	player_dir;
 }	t_config;
 
+void	init_config(t_config *config);
+int		check_args(int argc, char **argv);
+int		parse_file(char *filename, t_config *config);
 
 #endif
